e30f1c16ff5863d5d90661dac041694c12282a7a
jerrelllzw <lowjerrell47@gmail.com>
2024-08-13 20:44:44 +0800

    Refactor code

--- FastNews.py
+++ FastNews.py
@@ -1,11 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_food_news():
-    url = 'https://eatbook.sg/category/news/'  # URL to scrape
-
+def get_eatbook_food_news():
     try:
-        response = requests.get(url)
+        response = requests.get('https://eatbook.sg/category/news/')
         response.raise_for_status()  # Check if the request was successful
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while fetching the URL: {e}")
@@ -13,17 +11,17 @@ def get_food_news():
 
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Find all <div> elements with the class 'post-header' and extract <h2> tags with <a> tags
-    item_names = [
+    # Extract titles from post headers
+    titles = [
         a_tag.get_text(strip=True)
         for post_header in soup.find_all('div', class_='post-header')
         for h2_tag in post_header.find_all('h2')
         for a_tag in h2_tag.find_all('a')
     ]
 
-    return item_names
+    return titles
 
-# Example usage
-food_news = get_food_news()
+# Main
+food_news = get_eatbook_food_news()
 for item in food_news:
     print(item)
