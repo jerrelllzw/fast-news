17e1c62a7e74a34af366f9deed88a5d39b23a9ae
jerrelllzw <lowjerrell47@gmail.com>
2024-09-28 16:31:43 +0800

    Refactor code and add skeletons for property and bto

--- scraper.py
+++ scraper.py
@@ -12,30 +12,53 @@ def get_eatbook_food_news():
         return []
 
     soup = BeautifulSoup(response.text, 'html.parser')
-    titles = []
+    results = []
     for post_header in soup.find_all('div', class_='post-header'):
         for h2_tag in post_header.find_all('h2'):
             for a_tag in h2_tag.find_all('a'):
-                titles.append(a_tag.get_text(strip=True))
+                results.append(a_tag.get_text(strip=True))
 
-    return titles
+    return results
 
 def get_uniqlo_new_arrivals():
-    # Set up the WebDriver (you need to have a driver like ChromeDriver installed)
+    # Scrape using webdriver
     driver = webdriver.Chrome()
     driver.get('https://www.uniqlo.com/sg/en/special-feature/ut/collection-lineup')
-
-    # Let the page load fully
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-    # Find the specific div with id lineup_new_arrivals and extract titles under it
     new_arrivals_section = soup.select_one('div#lineup_new_arrivals div.lineup_list')
+    driver.quit()
 
-    titles = []
+    results = []
     if new_arrivals_section:
         for post_header in new_arrivals_section.select('div.lineup_itemBody > div.lineup_itemTtl'):
-            titles.append(post_header.get_text(strip=True))
+            results.append(post_header.get_text(strip=True))
 
+    return results
+
+def get_property_guru_listings():
+    try:
+        response = requests.get('https://www.propertyguru.com.sg/property-for-sale')
+        response.raise_for_status()
+    except requests.RequestException as e:
+        logging.error(f"An error occurred while fetching the URL: {e}")
+        return []
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    results = []
+    for listing in soup.find_all('div', class_='listing-card'):
+        results.append(listing.get_text(strip=True))
+
+    return results
+
+def get_bto_releases():
+    # Scrape using webdriver
+    driver = webdriver.Chrome()
+    driver.get('https://homes.hdb.gov.sg/home/finding-a-flat')
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
     driver.quit()
-    
-    return titles
+
+    results = []
+    for release in soup.find_all('div', class_='bto-release'):
+        results.append(release.get_text(strip=True))
+    print(results)
+    return results
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import logging
 import requests
-from scraper import get_eatbook_food_news, get_uniqlo_new_arrivals
+from scraper import get_eatbook_food_news, get_uniqlo_new_arrivals, get_property_guru_listings, get_bto_releases
 from config import TELEGRAM_TOKEN, CHAT_ID
 
 # In-Memory Storage
@@ -9,12 +9,7 @@ uniqlo_seen = set()
 property_seen = set()
 bto_seen = set()
 
-# Topics
-FAST_FOOD = {"McDonald’s", 'KFC', 'Popeyes', 'Burger King'}
-UNIQLO = {}
-PROPERTY = {}
-BTO = {}
-
+# Helper Functions
 def send_telegram_message(message):
     url = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage'
     data = {'chat_id': CHAT_ID, 'text': message}
@@ -24,24 +19,46 @@ def send_telegram_message(message):
     except requests.RequestException as e:
         logging.error(f"An error occurred while sending the Telegram message: {e}")
 
-def fetch_and_notify():
-    # Fast Food
-    fast_food_data = parse_data(get_eatbook_food_news(), FAST_FOOD)
+def fetch_and_notify_fast_food():
+    fast_food_data = [news for news in set(get_eatbook_food_news()) if any(topic in news for topic in {"McDonald’s", 'KFC', 'Popeyes', 'Burger King'})]
     fast_food_unseen = [news for news in fast_food_data if news not in fast_food_seen]
 
     if fast_food_unseen:
-        message = "New food news:\n" + "\n".join(fast_food_unseen)
+        message = "Fast Food News:\n" + "\n".join(fast_food_unseen)
         send_telegram_message(message)
         fast_food_seen.update(fast_food_unseen)
 
-    # Uniqlo
+def fetch_and_notify_uniqlo():
     uniqlo_data = get_uniqlo_new_arrivals()
-    uniqlo_unseen = [news for news in uniqlo_data if news not in uniqlo_seen]
+    uniqlo_unseen = [arrivals for arrivals in uniqlo_data if arrivals not in uniqlo_seen]
 
     if uniqlo_unseen:
-        message = "New uniqlo news:\n" + "\n".join(uniqlo_unseen)
+        message = "Uniqlo New Arrivals:\n" + "\n".join(uniqlo_unseen)
         send_telegram_message(message)
         uniqlo_seen.update(uniqlo_unseen)
 
-def parse_data(news_list, test):
-    return [news for news in set(news_list) if any(topic in news for topic in test)]
+def fetch_and_notify_property():
+    property_data = get_property_guru_listings()
+    property_unseen = [listings for listings in property_data if listings not in property_seen]
+
+    if property_unseen:
+        message = "Property Listings:\n" + "\n".join(property_unseen)
+        send_telegram_message(message)
+        uniqlo_seen.update(property_unseen)
+
+def fetch_and_notify_bto():
+    bto_data = get_bto_releases()
+    bto_unseen = [releases for releases in bto_data if releases not in bto_seen]
+
+    if bto_unseen:
+        message = "BTO Releases:\n" + "\n".join(bto_unseen)
+        send_telegram_message(message)
+        bto_seen.update(bto_unseen)
+
+
+# Main Function
+def fetch_and_notify():
+    # fetch_and_notify_fast_food()
+    # fetch_and_notify_uniqlo()
+    # fetch_and_notify_property()
+    fetch_and_notify_bto()
