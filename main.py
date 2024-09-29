5a23c84815c6ccaea6b92fa44088ff92295d3bda
jerrelllzw <lowjerrell47@gmail.com>
2024-09-29 20:14:46 +0800

    Change in memory storage to file storage

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import time
-from utils import fetch_and_notify
+from utils import fetch_and_notify_fast_food, fetch_and_notify_uniqlo, fetch_and_notify_property, fetch_and_notify_bto
 
 def main():
-    while True:
-        fetch_and_notify()
-        time.sleep(60)
+    """Fetches data from all sources and sends notifications for new items."""
+    fetch_and_notify_fast_food()
+    fetch_and_notify_uniqlo()
+    fetch_and_notify_property()
+    fetch_and_notify_bto()
 
 if __name__ == "__main__":
     main()
