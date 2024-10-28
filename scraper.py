34ef875bfcdf19b1564d7fd0b8c31730dcd9679d
jerrelllzw <lowjerrell47@gmail.com>
2024-10-28 10:45:56 +0800

    Add headless mode

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 from typing import List, Set
@@ -26,8 +27,10 @@ def get_soup_from_url(url: str) -> BeautifulSoup:
         return BeautifulSoup("", 'html.parser')
     return BeautifulSoup(response.text, 'html.parser')
 
-def get_soup_from_webdriver(url: str, wait_time: int = 3) -> BeautifulSoup:
-    with webdriver.Chrome(service=Service(ChromeDriverManager().install())) as driver:
+def get_soup_from_webdriver(url: str, wait_time: int = 1) -> BeautifulSoup:
+    options = Options()
+    options.add_argument("--headless")
+    with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options) as driver:
         driver.get(url)
         time.sleep(wait_time)
         soup = BeautifulSoup(driver.page_source, 'html.parser')
